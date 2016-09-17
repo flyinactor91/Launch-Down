181ccd73a5119a91dbfed41cf2abae1081466d3c
Michael duPont <michael@mdupont.com>
2016-09-17 17:58:54 -0400

    Button handler, tick timeout, liftoff

--- js/handlers.js
+++ js/handlers.js
@@ -1,16 +1,37 @@
-var seconds = 90;
+var gameclock = 10;
 
 function updateTime() {
-    var minute = Math.floor(seconds / 60);
-    var second = seconds % 60;
+    var minute = Math.floor(gameclock / 60);
+    var second = gameclock % 60;
+
+    if(minute<0){minute = "00"}
+    else if(minute<10){minute = "0" + minute}
+    
+    if(second<0){second = "00"}    
+    else if(second<10){second = "0" + second}
+
     console.log(minute + ':' + second);
-    document.getElementById('clock').innerHTML = minute + ':' + second;
-    seconds -= 1;
+    document.getElementById('clock').innerHTML = 'T-' + minute + ':' + second;
+    gameclock -= 1;
+}
+
+function liftoff() {
+    console.log('LIFTOFF');
 }
 
 function timeHandler() {
     //set clock
     updateTime();
+    //If time has run out, stop interval and run liftoff code
+    if (0 > gameclock) {
+        clearInterval(tick);
+        liftoff();
+    }
+}
+
+function powerHandler(ptype) {
+    if (0 > gameclock) return;
+    console.log(ptype);
 }
 
 var tick = setInterval(timeHandler, 1000);
