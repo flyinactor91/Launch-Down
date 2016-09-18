0660ead2a1de155faacb01127f21d28cbd986ffc
randompast <matteroflight@gmail.com>
2016-09-18 15:44:41 -0400

    Updated Sounds

--- js/powers.js
+++ js/powers.js
@@ -6,28 +6,30 @@ var powersettings = {
             "abiltext": "50% chance of exploding in-flight",
             "abilcost": 1.5,
             "animationName":'Thunderstorm',
-            "sound":"../art/Sounds/Thunder_HD-Mark_DiAngelo-587966950.mp3"
+            "sound":"../art/Sounds/thunder.mp3"
         },
         "Animal Attack": {
             "visibility": 2,
             "damage": 1,
             "abiltext": null,
             "abilcost": 1,
-            "animationName":'AnimalAttack',
+            "animationName":'AnimalAttack',"sound":"../art/Sounds/birds.mp3"
         },
         "Civilian Interference": {
             "visibility": 4,
             "damage": 1,
             "abiltext": "20% chance of exploding in-flight",
             "abilcost": 1.2,
-            "animationName":'CivilianInterference',
+            "animationName":'CivilianInterference',"sound":"../art/Sounds/Teacher Charlie Brown Talking-SoundBible.com-1454295165.mp3"
+
         },
         "Excessive Frost": {
             "visibility": 4,
             "damage": 3,
             "abiltext": "25% chance of exploding on launch pad",
             "abilcost": 1.25,
-            "animationName":'ExcessiveFrost',
+            "animationName":'ExcessiveFrost',"sound":"../art/Sounds/freeze.mp3"
+
         }
     },
     "Mechanical": {
@@ -36,28 +38,32 @@ var powersettings = {
             "damage": 5,
             "abiltext": null,
             "abilcost": 1,
-            "animationName":'FuelingError',
+            "animationName":'FuelingError',"sound":"../art/Sounds/fueling_error.mp3"
+
         },
         "Launch Pad Leak": {
             "visibility": 5,
             "damage": 5,
             "abiltext": null,
             "abilcost": 1,
-            "animationName":'LaunchPadLeak',
+            "animationName":'LaunchPadLeak',"sound":"../art/Sounds/Chamber Decompressing-SoundBible.com-1075404493.mp3"
+
         },
         "Rocket Valve Failure": {
             "visibility": 3,
             "damage": 5,
             "abiltext": null,
             "abilcost": 1,
-            "animationName":'RocketValveFailure',
+            "animationName":'RocketValveFailure',"sound":"../art/Sounds/Air Leaking Out Sound-SoundBible.com-714635080.mp3"
+
         },
         "Malfunctioning Sensors": {
             "visibility": 1,
             "damage": 3,
             "abiltext": "20% chance of exploding on launch pad",
             "abilcost": 1.2,
-            "animationName":'MalfunctioningSensors',
+            "animationName":'MalfunctioningSensors',"sound":"../art/Sounds/ie_Short_Cow_MooDTMF_Tones-KevanGC-494166652.mp3"
+
         }
     },
     "Human Factors": {
@@ -66,28 +72,31 @@ var powersettings = {
             "damage": 3,
             "abiltext": "Less likely to notice environmental issues",
             "abilcost": 1.5,
-            "animationName":'LaunchTeamDistraction',
+            "animationName":'LaunchTeamDistraction',"sound":"../art/Sounds/16mm Film Projector-SoundBible.com-1297668862.mp3"
         },
         "Flight Crew Distraction": {
             "visibility": 4,
             "damage": 5,
             "abiltext": null,
             "abilcost": 1,
-            "animationName":'FlightCrewDistraction',
+            "animationName":'FlightCrewDistraction',"sound":"../art/Sounds/Water Drop Sound High-SoundBible.com-1387792987.mp3"
+
         },
         "Quality Assurance Failure": {
             "visibility": 2,
             "damage": 3,
             "abiltext": "Less likely to notice mechanical issues",
             "abilcost": 1.5,
-            "animationName": 'QualityAssuranceFailure',
+            "animationName": 'QualityAssuranceFailure',"sound":"../art/Sounds/chipmunk.mp3"
+
         },
         "Management Pressure": {
             "visibility": 3,
             "damage": 1,
             "abiltext": "25% chance of guarenteed launch",
             "abilcost": 1.75,
-            "animationName": "ManagementPressure",
+            "animationName": 'ManagementPressure',"sound":"../art/Sounds/Angry Chipmunk-SoundBible.com-980210050.mp3"
+
         }
     }
 }
