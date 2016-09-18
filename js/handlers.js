63c3080bc324b5ff1f5ee9cd9f48b1bd80613416
Cascode6 <caseylfaist@gmail.com>
2016-09-17 21:37:51 -0400

    Merge animations to handlers

--- js/handlers.js
+++ js/handlers.js
@@ -49,6 +49,37 @@ function timeHandler() {
 
 //Power-based handler functions
 
+
+// image animation function and mapping
+
+var animationMappings = {
+    "Environment": {"start":["0px", "0px"], "end":["400px", "400px"],},
+    "Mechanical": {"start":["0px", "0px"], "end":["400px", "400px"],},
+    "Human": {"start":["0px", "0px"], "end":["400px", "400px"],},
+}
+
+
+
+function animate(button) {
+    document.getElementById(button).style.visibility = 'visible';
+    var elem = document.getElementById(button);   
+    var begin = animationMappings[button]["start"];
+    var end = animationMappings[button]["end"];
+    var id = setInterval(frame, 12);
+    function frame() {
+        if (begin == end) {
+        clearInterval(id);
+        } else {
+        begin++; 
+        elem.style.top = begin + 'px'; 
+        elem.style.left = begin + 'px'; 
+        }
+  }
+}
+
+
+
+
 // checks player visibility on button click
 function updateVisibility(Vis) {
     if (levelVisibility <= 5) {
